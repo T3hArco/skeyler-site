e6b07e5bbf597adf5f03dc18743e42670fc75c63
obst <obstipator@gmail.com>
2014-02-08 13:17:48 -0500

    stylin

--- views/common/header.php
+++ views/common/header.php
@@ -34,12 +34,12 @@
 <div id="container">
   <div class="header">
     <h1><strong>Skeyler</strong> <span>Gmod Community</span></h1>
-    <ul class="topNav">
-      <li <?php echo ($local['navigationItem'] == 'news' ? 'class = "selected"' : ''); ?>><a href="/blog.php">News</a></li>
-      <li <?php echo ($local['navigationItem'] == 'forums' ? 'class = "selected"' : ''); ?>><a href="/forums.php">Forums</a></li>
-      <li <?php echo ($local['navigationItem'] == 'servers' ? 'class = "selected"' : ''); ?>><a href="/servers.php">Servers</a></li>
-      <li <?php echo ($local['navigationItem'] == 'stats' ? 'class = "selected"' : ''); ?>><a href="/stats.php">Stats</a></li>
-    </ul>
+    <ul class="topNav"><!--
+      --><li <?php echo ($local['navigationItem'] == 'news' ? 'class = "selected"' : ''); ?>><a href="/blog.php">News</a></li><!--
+      --><li <?php echo ($local['navigationItem'] == 'forums' ? 'class = "selected"' : ''); ?>><a href="/forums.php">Forums</a></li><!--
+      --><li <?php echo ($local['navigationItem'] == 'servers' ? 'class = "selected"' : ''); ?>><a href="/servers.php">Servers</a></li><!--
+      --><li <?php echo ($local['navigationItem'] == 'stats' ? 'class = "selected"' : ''); ?>><a href="/stats.php">Stats</a></li><!--
+    --></ul>
   </div>
 
   <?php Notice::writeNotices(); ?>
